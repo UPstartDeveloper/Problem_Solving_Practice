6fab3124bfab6f5cb2778aad6f4a5f5c6f2c5c9a
= <zainr7989@gmail.com>
2020-03-13 11:24:17 -0700

    Update documentation on staricase solution.

--- staircase.py
+++ staircase.py
@@ -1,23 +1,39 @@
-# Solving the Hacker Rank problem found here: https://www.hackerrank.com/challenges/staircase/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=7-day-campaign
+# Solving the Hacker Rank problem found here:
+# https://www.hackerrank.com/challenges/staircase/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=7-day-campaign
 
 
-def staircase(hashtags):
-    '''Given an integer number of hashtags, return a right aligned staircase of
+def staircase(num_steps):
+    """Given an integer number of num_steps, return a right aligned staircase of
        those characters.
 
-    '''
+       Parameters:
+       num_steps(int): a value representing the number of steps there are in
+                      the staircase. This will also be the same as the number
+                      of num_steps symbols on the last step (the line printed
+                      last to standard output).
+
+       Returns:
+       None
+
+    """
+    # initialize a count of symbols to print on each step of the staircase
     quota = 1
-    for i in range(hashtags):
+    # build the string representing each step of the staircase
+    for i in range(num_steps):
         line = ""
-        for j in range(hashtags):
-            if hashtags - j <= quota:
+        for j in range(num_steps):
+            # print spaces until it is time to add # to the output line
+            if num_steps - j <= quota:
                 line += "#"
             else:
                 line += " "
+        # display the output
         print(line)
+        # increment the number of symbols needed for the next iteration
         quota += 1
     return None
 
 
 if __name__ == "__main__":
+    # test input of 4 steps
     staircase(4)
