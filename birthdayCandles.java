dd1b2e2f393943d06bb09b2f7b5bbd226931d1d9
= <zainr7989@gmail.com>
2020-04-03 22:49:47 -0400

    Init Birthday Cake Candles solution.

---
+++ birthdayCandles.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+    /*************************************************************************
+    // Solving "Brithday Cake Candles" problem on Hacker Rank:
+    // https://www.hackerrank.com/challenges/birthday-cake-candles/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+    **************************************************************************/
+    /* birthdayCakeCandles
+     * Pre: int[] candleHeights is an array of the heights of brithday candles
+     * Post: int - represents the number of candles that will go out
+     *       by storing the occurences of the highest candle height
+     */
+     public static int birthdayCakeCandles(int[] candlesHeights) {
+
+
+     }
+     /* Main method
+      * Pre: none
+      * Post: none
+      */
+     public static void main(String[] args) throws IOException {
+
+         }
+
+     }
