f50171b51ea310b3f20ad435f285cdc26f276536
= <zainr7989@gmail.com>
2020-04-08 17:18:58 -0400

    Add class 4 notes.

---
+++ day_four.py
@@ -0,0 +1,56 @@
+'''
+In class work:
+
+Coding Conventions
+1. Python: after else, put a comment saying the condition I know must be True
+if x > 4:
+    do_something()
+else: # x <= 4
+    do_something_else()
+
+2. Python 3, you can put an => saying what the return value in a function is
+    def __add__(self, other: 'Position') -> 'Position':
+
+3. Java: using camelCase
+
+4. Indentation: when you make {} that's longer than one
+    for {
+        hjgjjg
+    }
+5. semantic variable, argument (and file) names
+    - it should say "what it is"
+    - coefficient is k
+    - string is str
+    - array -> arr
+    - Linked List -> ll
+
+6. Python: line length less than 80 characters
+
+7. Whitespace between functions:
+    2 if not in a class, 1 if in a class
+
+8. Leave one blank line at the end of a Python script
+
+9. Python: use TitleCase in naming classes
+
+10. HTML: indent nested tags
+
+11. HTML: declare document type at top of file
+<!DOCTYPE html>
+
+12. Big O - clearly
+
+
+FizzBuzz errors:
+    1. not using camelCase in function name,
+    2. not using else if
+    3. missing semi colon
+    4. unnecessary semi colon in for loop
+    5. not camelCase in targetnum
+    6. Needs to use &&, not &
+    7. Mixing ES6 and non ES6 syntax (var, let)
+
+Stack class errors:
+
+
+'''
