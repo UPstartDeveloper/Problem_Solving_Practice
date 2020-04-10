3874ef910e90a8b67710be1f1950db3962df601a
= <zainr7989@gmail.com>
2020-04-10 17:33:20 -0400

    Fix solution to substrings problem.

--- SPD_hw_4_problem_2.py
+++ SPD_hw_4_problem_2.py
@@ -3,16 +3,23 @@ Find the longest substring of unique letters in a given string of n letters.
 """
 
 
+def get_substrings(text):
+    '''Returns list of all unique substrings (letters only) in text.'''
+    index = 0
+    substrings = list()
+    next_str = ''
+    next_char = text[index]
+    while next_char not in next_str and index < len(text):
+        next_str += next_char
+        substrings.append(next_str)
+        index += 1
+        next_char = text[index]
+    return substrings
+
+
 def find_longest_substring(text):
     # build a list of substrings
-    substrings = list()
-    index = 0
-    while index < len(text):
-        next_substring = [
-            text[i] for i in range(len(text)) if text[i] not in text[:i]
-        ]
-        substrings.append(''.join(next_substring))
-        index += len(next_substring)
+    substrings = get_substrings(text)
     # find the longest using linear search
     longest_length, index_of_longest = 0, 0
     for i in range(len(substrings)):
@@ -24,5 +31,5 @@ def find_longest_substring(text):
 
 
 if __name__ == '__main__':
-    text = "I feel sleepy"
+    text = "sleepy"
     print(find_longest_substring(text))
