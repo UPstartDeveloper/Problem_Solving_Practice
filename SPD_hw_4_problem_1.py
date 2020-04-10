77a3a40daeee1521fe9b4c925d7cf93904a8deff
= <zainr7989@gmail.com>
2020-04-10 16:32:42 -0400

    Add function to choose k largest.

--- SPD_hw_4_problem_1.py
+++ SPD_hw_4_problem_1.py
@@ -43,19 +43,58 @@ def build_histogram(text):
     return histogram
 
 
-def get_frequent_words(text, k):
-    '''A variation of the n-choose-k problem, which also involves sorting.
+def find_min_and_its_index(largest_pairs):
+    '''Linear search for the minimum word-count pair.'''
+    current_min_index, current_min = 0, largest_pairs[0][1]
+    for i in range(1, len(largest_pairs)):
+        next_count = largest_pairs[i][1]
+        if next_count < current_min:
+            current_min = next_count
+            # we keep track of the index of the overall tuple
+            current_min = i
+    return current_min_index, current_min
+
+
+def find_largest(word_counts, count):
+    """Iterates over a list of tuples, i.e. [('word': count_num)], and returns
+       the subset of tuples with the largest counts, size count elements.
+       Return value is given in decreasing sorted order.
+
+    """
+    # start by pre-loading the first pairs in the list, for as many as count
+    largest_pairs = word_counts[:3]
+    # keep track of the smallest pair in this list (linear search)
+    current_min_index, current_min = find_min_and_its_index(largest_pairs)
+    # find the true count-many word pairs in the list
+    for i in range(3, len(word_counts)):
+        # compare the next word count pair, to the minimum of the largest
+        next_pair = word_counts[i]
+        next_count = word_counts[i][1]
+        # replace the current minimum in largest pairs, if necessary
+        if next_count > current_min:
+            largest_pairs[current_min_index] = next_pair
+            current_min_index, current_min = (
+                find_min_and_its_index(largest_pairs))
+    return largest_pairs
+
+
+def get_frequent_words(text, count):
+    """A variation of the n-choose-k problem, which also involves sorting.
        Assumptions:
         1) the text contains alphabetic characters, with spaces in between
         2) Each word type has a distinct number of tokens associated with it
         3) word types are case insensitive, i.e. "one" = "One"
-    '''
+
+    """
     # build a distribution using a hashtable data type
     histogram = build_histogram(text)
-    print(histogram)
+    # find the k-largest mappings
+    largest = find_largest(histogram.items(), count)
+    # sort the largest_pairs in descending order
+    # return just the words
 
 
 if __name__ == '__main__':
     text = 'One fish two fish red fish blue fish'
-    k = 3  # expected k <= number of word types
-    get_frequent_words(text, k)
+    count = 3  # expected k <= number of word types
+    get_frequent_words(text, count)
