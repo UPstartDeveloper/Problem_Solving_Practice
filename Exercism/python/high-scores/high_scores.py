da129d870eb59b26af060a83199c2a0b44e9ebc9
= <zainr7989@gmail.com>
2020-04-16 09:30:01 -0400

    Improve personal_best to handle good, bad, and edge case inputs.

--- Exercism/python/high-scores/high_scores.py
+++ Exercism/python/high-scores/high_scores.py
@@ -15,15 +15,28 @@ def personal_best(scores):
 
        Only one score will be returned by this function.
 
+       I am not allowed to mutate the input.
+
        Problem was provided by Exercism:
        https://exercism.io/my/solutions/b50ca3f9f3d54e4fa958ff970aea2489
 
     """
-    # init arbitrarily low value - equals -9223372036854775807, or 2**31-1
-    max = -(sys.maxsize - 1)
+    # take all unique numerical values into a separate list
+    scores_set = list()
     for score in scores:
+        if (isinstance(score, int) or isinstance(score, float)):
+            scores_set.append(score)
+    # if there were no numrical scores, exit function
+    if len(scores_set) == 0:
+        return "Top score is unavailable right now."
+    # loop through the set of candidate scores
+    max = scores_set[0]
+    for i in range(len(scores_set)):
+        score = scores_set[i]
+        # if the score is the largest we've seen so far, it's the maximum
         if score > max:
             max = score
+    # return the max score
     return max
 
 
@@ -32,12 +45,23 @@ def personal_top_three(scores):
 
 
 if __name__ == '__main__':
+    # personal_best function
     # Good inputs
     scores = [4, 5, 6, -9, 11]
     assert personal_best(scores) == 11
 
-    scores = [2, 2.3, -5]
-    assert personal_best(scores) == 2.3
+    scores = [2.3, 2.0, -5]
+    assert personal_best(scores) == 2.3, f'Returned: {personal_best(scores)}'
     # Bad Inputs
+    scores = [2.3, 2.0, 'birthday party']
+    assert personal_best(scores) == 2.3
+
+    scores = ['birthday party']
+    assert personal_best(scores) == "Top score is unavailable right now."
 
     # Edge Cases
+    scores = []
+    assert personal_best(scores) == "Top score is unavailable right now."
+
+    scores = [5, 5, 5, 5]
+    assert personal_best(scores) == 5
