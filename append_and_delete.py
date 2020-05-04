362c9c14142ce9ede8bb5a0c4ad16f895e3d65cf
= <zainr7989@gmail.com>
2020-05-04 16:31:49 -0400

    Additional practice.

--- append_and_delete.py
+++ append_and_delete.py
@@ -10,42 +10,50 @@ def appendAndDelete(s, t, k):
     # make the strings into lists, so they easier to work with
     s = list(s)
     t = list(t)
-    # iterate over strings to see how much of a common prefix they share
-    index = 0
-    while index < len(s) and index < len(t):
-        letter_s, letter_t = s[index], t[index]
-        if letter_s != letter_t:
-            break
-        index += 1
-    # delete letters in s from this index onwards
-    for i in range(index, len(s)):
-        if k > 0 and i < len(s):
-            s[i] = ''
-            # print(s, i)
-            k -= 1
-    # append letters to s from t, also starting at this index
-    # print(f'k: {k}')
-    for i in range(index, len(t)):
-        if k > 0 and i < len(t):
-            s.append(t[i])
-            k -= 1
-    # see if the strings are equal now, and make decision
-    s = ''.join(s)
-    t = ''.join(t)
-    if s == t and k >= 0:
-        return 'Yes'
+    # checks lengths to know when to be inefficent
+    input_length = len(s)
+    target_length = len(t)
+    diff_length = target_length - input_length
+    if (diff_length > 0) and (diff_length < k):
+        print('')
     else:
-        # print(f'S: {s}')
-        # print(f't: {t}')
-        return 'No'
+        # iterate over strings to see how much of a common prefix they share
+        index = 0
+        while index < len(s) and index < len(t):
+            letter_s, letter_t = s[index], t[index]
+            if letter_s != letter_t:
+                break
+            index += 1
+        # delete letters in s from this index onwards
+        for i in range(index, len(s)):
+            if k > 0 and i < len(s):
+                s[i] = ''
+                # print(s, i)
+                k -= 1
+        # append letters to s from t, also starting at this index
+        # print(f'k: {k}')
+        for i in range(index, len(t)):
+            if k > 0 and i < len(t):
+                s.append(t[i])
+                k -= 1
+        # see if the strings are equal now, and make decision
+        s = ''.join(s)
+        t = ''.join(t)
+        print(f'S: {s}')
+        print(f't: {t}')
+        print(f'k: {k}')
+        if s == t and k == 0:
+            return 'Yes'
+        else:
+            return 'No'
 
 
 if __name__ == '__main__':
-    s = 'y'
+    s = 'aaaaaaaaaa'
 
-    t = 'yu'
+    t = 'aaa'
 
-    k = 2
+    k = 7
 
     result = appendAndDelete(s, t, k)
     print(result)
---
+++ complexity_lesson.py
@@ -0,0 +1,47 @@
+'''
+Given a sorted array, find the index of the first and last
+occurrence of a given element. If the given element is not found in the array, report that.
+
+- integers, duplicates are allowed
+
+element = 2
+[2, 5, 6, 2]
+
+output - [0, 3]
+
+0: 2  --> 0, last_index = first_index + (number of occurences - 1)
+
+Pseudocode
+dict to map allow the value to its indices
+index into the first and last
+
+distribution =
+{
+  0: 1
+  1: 1
+}
+
+
+index 1
+num   2
+'''
+
+
+def find_indices(array, element):
+    distribution = {}
+    for index, num in enumerate(array):
+        if num == element and (len(distribution.keys()) == 0):
+            distribution[index] = 1
+        elif num == element:
+            for index in distribution.keys():
+                distribution[index] += 1
+
+    if len(distribution.keys()) == 1:
+        for index in distribution.keys():
+            return [index, index + (distribution[index] - 1)]
+    else:
+        return "Element not in list."
+
+
+if __name__ == '__main__':
+    print(find_indices([2, 2, 5, 6], 2))
