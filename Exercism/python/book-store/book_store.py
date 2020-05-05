f2c3ce8a82c33da9bb3e835b85cf4e9cd2fa09d5
= <zainr7989@gmail.com>
2020-05-05 15:19:51 -0400

    Continue editing book store.

--- Exercism/python/book-store/book_store.py
+++ Exercism/python/book-store/book_store.py
@@ -1,22 +1,4 @@
 def total(basket):
-    '''# form a distibution of the copies of each book being bought
-    book_orders = {}
-    for book in basket:
-        if book in book_orders:
-            book_orders[book] += 1
-        else:  # book not in book_orders
-            books_orders[book] = 1
-    # form the minimum number of sets from the population of books in basket
-    pass
-    # use the lengths of each set to calculate largest discount subtotal
-    cost_one_book = 8
-    order_discounts = {
-        1: 0,
-        2: .05,
-        3: .10,
-        4: .20,
-        5: .25
-    }'''
     # make the minimum number of sets from the basket
     book_orders = list()
     for j in range(len(basket)):
@@ -26,15 +8,40 @@ def total(basket):
             collection = book_orders[i]
             if book not in collection:
                 collection.add(book)
-                print(book_orders)
+                # print(book_orders)
                 break
         else:
             new_order = set([book])
             book_orders.append(new_order)
-            # print(book_orders)
-        # adding book as part of a new set
+    # redistribution of books, so average number of books/order increases
+    for i in range(len(book_orders)):
+        if i < len(book_orders):
+            order = book_orders[i]
+            for j in range(len(book_orders)):
+                if i != j:
+                    if j < len(book_orders):
+                        other_order = book_orders[j]
+                        print(book_orders)
+                        # if this order has no unique values versus another order
+                        if (len(order - other_order) == 0 and not
+                                len(order) >= len(other_order)):
+                            print(f'Order: {order}')
+                            print(f'Other order: {other_order}')
+                            # transfer of book between orders
+                            unique_books = other_order - order
+                            book_to_give = unique_books.pop()
+                            order.add(book_to_give)
+                            other_order.remove(book_to_give)
+                            # reassign into the book_orders array
+                            print(i, j)
+                            book_orders[i], book_orders[j] = order, other_order
+                            '''del book_orders[i + 1]
+                            if i < j:
+                                del book_orders[j]
+                            else:
+                                del book_orders[j + 1]
+                            print(book_orders)'''
     # use the lengths of each set to calculate discount percentage
-    # print(order_sets)
     cost_one_book = 8
     order_discounts = {
         0: 0,
@@ -46,8 +53,6 @@ def total(basket):
     }
     # calculate subtotal
     total_cost = 0
-    # print(total_cost)
-    # total_cost = total_cost - (total_cost * order_discounts[num_orders])
     for i in range(len(book_orders)):
         order_length = len(book_orders[i])
         discount_rate = order_discounts[order_length]
