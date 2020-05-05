c91f45e14130fc5169ec7d56f8d34c68b4af4031
= <zainr7989@gmail.com>
2020-05-05 12:02:41 -0400

    Convert matrix string to list of columns.

--- Exercism/python/matrix/matrix.py
+++ Exercism/python/matrix/matrix.py
@@ -1,4 +1,9 @@
 class Matrix:
+    """Assumptions based off the problem description, and test cases on
+       Exercism: matrix_string only contains positive integers, and the matrix
+       has square dimensions.
+
+    """
     def __init__(self, matrix_string):
         # make a list of the rows
         matrix_lines = matrix_string.splitlines()
@@ -10,8 +15,16 @@ class Matrix:
             row = [int(num) for num in line]
             self.rows.append(row)
 
+        # make a list of columns
+        self.cols = list()
+        for i in range(len(row)):
+            # iterate over all elements in the same column
+            col = [row[i] for row in self.rows]
+            self.cols.append(col)
+
     def row(self, index):
-        return self.rows[index]
+        print(self.rows)
+        return self.rows[index - 1]
 
     def column(self, index):
-        pass
+        return self.cols[index - 1]
