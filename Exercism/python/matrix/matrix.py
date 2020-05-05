7844a9f5993f064e340490a1b379d0bc3a5b20cd
= <zainr7989@gmail.com>
2020-05-05 11:55:26 -0400

    Convert matrix string into 2D array.

--- Exercism/python/matrix/matrix.py
+++ Exercism/python/matrix/matrix.py
@@ -1,9 +1,17 @@
 class Matrix:
     def __init__(self, matrix_string):
-        pass
+        # make a list of the rows
+        matrix_lines = matrix_string.splitlines()
+        self.rows = list()
+        for line in matrix_lines:
+            # split individual number strings
+            line = line.split()
+            # convert to numbers, and append to overall list of rows
+            row = [int(num) for num in line]
+            self.rows.append(row)
 
     def row(self, index):
-        pass
+        return self.rows[index]
 
     def column(self, index):
         pass
