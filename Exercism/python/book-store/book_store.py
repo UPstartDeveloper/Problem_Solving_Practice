11e4bd43d7bb2733befa0370d9932467dbddb1aa
= <zainr7989@gmail.com>
2020-05-05 15:29:44 -0400

    Continue editing book store.

--- Exercism/python/book-store/book_store.py
+++ Exercism/python/book-store/book_store.py
@@ -17,24 +17,23 @@ def total(basket):
     for i in range(len(book_orders)):
         if i < len(book_orders):
             order = book_orders[i]
-            for j in range(len(book_orders)):
-                if i != j:
-                    if j < len(book_orders):
-                        other_order = book_orders[j]
-                        print(book_orders)
-                        # if this order has no unique values versus another order
-                        if (len(order - other_order) == 0 and not
-                                len(order) >= len(other_order)):
-                            print(f'Order: {order}')
-                            print(f'Other order: {other_order}')
-                            # transfer of book between orders
-                            unique_books = other_order - order
-                            book_to_give = unique_books.pop()
-                            order.add(book_to_give)
-                            other_order.remove(book_to_give)
-                            # reassign into the book_orders array
-                            print(i, j)
-                            book_orders[i], book_orders[j] = order, other_order
+            j = i + 1
+            if j < len(book_orders):
+                other_order = book_orders[j]
+                print(book_orders)
+                # if this order has no unique values versus another order
+                if (len(order - other_order) == 0 and not
+                        len(order) >= len(other_order)):
+                    print(f'Order: {order}')
+                    print(f'Other order: {other_order}')
+                    # transfer of book between orders
+                    unique_books = other_order - order
+                    book_to_give = unique_books.pop()
+                    order.add(book_to_give)
+                    other_order.remove(book_to_give)
+                    # reassign into the book_orders array
+                    print(i, j)
+                    book_orders[i], book_orders[j] = order, other_order
                             '''del book_orders[i + 1]
                             if i < j:
                                 del book_orders[j]
