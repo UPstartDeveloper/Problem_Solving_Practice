83f1aebfca2afdc1abe00a45709fc3491c951ac7
= <zainr7989@gmail.com>
2020-05-13 10:47:18 -0400

    Init rectangle love solution.

---
+++ love_rectangle.py
@@ -0,0 +1,19 @@
+"""
+Solving the following problem from Interview Cake:
+https://www.interviewcake.com/question/python/rectangular-love
+"""
+
+"""
+1. Restate the problem:
+Ok, so you want me to find the overlap between these two rectangles, correct?
+
+2. Clarifying Questions:
+Alright, so I see in the example given that the rectangles are in the upper
+    right corner of the Cartesian plane. Will the values for the 'left_x' and
+    'bottom_y' keys always be positive? Or can these rectangles sometimes go
+    into the other corners of the Cartesian plane?
+
+What shall be returned if there is no intersection between the two rectangles?
+    Would just an empty dict() object (e.g. {}) suffice?
+
+"""
