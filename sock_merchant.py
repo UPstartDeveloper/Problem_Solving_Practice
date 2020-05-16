5ef3664860846e324181d3e35513872f76a33fe9
= <zainr7989@gmail.com>
2020-05-15 22:15:14 -0400

    Add solution to sock merchant problem.

---
+++ sock_merchant.py
@@ -0,0 +1,20 @@
+"""
+Solving problem listed on Hacker Rank:
+https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+"""
+
+
+def solution(socks):
+    '''Return an integer representing the number of pairs of matching socks.'''
+    # build a histogram of the socks
+    sock_colors = {}
+    for sock in socks:
+        if sock not in sock_colors.keys():
+            sock_colors[sock] = 1
+        else:
+            sock_colors[sock] += 1
+    # count the amount of pairs
+    pairs = 0
+    for count in sock_colors.values():
+        pairs += count // 2
+    return pairs
