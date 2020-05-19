7f07b6d24366fa08ff5ab5d76a2b2c70854e70e6
= <zainr7989@gmail.com>
2020-05-18 23:00:23 -0400

    Try solving counting valleys using a queue.

--- counting_valleys.py
+++ counting_valleys.py
@@ -9,11 +9,15 @@ start increment a down_count once we start seeing D's
     - if down_count == 0, then valley_count += 1
 
     return valley count
+
+Idea #2
+enqueue D steps, dequeue when we see U steps, increment valley count when
+the queue has a length of 0
 """
 
 
 def counting_valleys(n, s):
-    altitude, down_count, valley_count = 0, 0, 0
+    """altitude, down_count, valley_count = 0, 0, 0
     for i in range(len(s)):
         letter = s[i]
         if letter == 'U' and altitude < 0:
@@ -25,6 +29,21 @@ def counting_valleys(n, s):
             altitude -= 1
         print(altitude)
         if altitude == 0:
+            valley_count += 1
+
+    return valley_count"""
+    steps = list()
+    for i in range(len(s)):
+        letter = s[i]
+        if letter == 'U' and len(steps) > 0:
+            steps.pop()
+            # if down_count > 0:
+            # down_count -= 1
+        elif letter == 'D':
+            # down_count += 1
+            steps.append(letter)
+        print(steps)
+        if len(steps) == 0:
             valley_count += 1
 
     return valley_count
