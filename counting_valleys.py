b8e8fb80110ed219bafe4ad0b63fa4862f394e38
= <zainr7989@gmail.com>
2020-05-18 23:28:07 -0400

    Attempt counting valleys with while loop.

--- counting_valleys.py
+++ counting_valleys.py
@@ -2,7 +2,7 @@
 Hacker Rank problem found on:
 https://www.hackerrank.com/challenges/counting-valleys/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
 
-Idea #1:
+Idea #1: using a for loop
 start increment a down_count once we start seeing D's
     - if we see a D, the down_count += 1
     - if we see a U, the down_count -= 1
@@ -10,9 +10,20 @@ start increment a down_count once we start seeing D's
 
     return valley count
 
-Idea #2
+Idea #2: using a queue
 enqueue D steps, dequeue when we see U steps, increment valley count when
 the queue has a length of 0
+
+Idea #3: using a while loop
+A: traverse over the string
+B: track the altitude overall
+C: if we see a 'D':
+    start keeping track of the steps taken in the valley
+    if the valley_depth == altitude, then increment valley_count and exit the
+        inner loop
+D: fast forward the index by howver many steps just taken in the inner loop, or
+    just by 1 if we didn't enter the loop
+E: after the traversal, return the valley_count
 """
 
 
@@ -31,7 +42,7 @@ def counting_valleys(n, s):
         if altitude == 0:
             valley_count += 1
 
-    return valley_count"""
+    return valley_count
     steps = list()
     for i in range(len(s)):
         letter = s[i]
@@ -46,4 +57,30 @@ def counting_valleys(n, s):
         if len(steps) == 0:
             valley_count += 1
 
+    return valley_count"""
+    altitude, valley_count, i = 0, 0, 0
+    while i < len(s):
+        letter = s[i]
+        if letter == 'U':
+            altitude += 1
+        elif letter == 'D':
+            altitude -= 1
+        if altitude <= 0:
+            valley_depth, j = altitude * -1, i + 1
+            while j < len(s):
+                letter_of_valley = s[j]
+                if letter_of_valley == 'U':
+                    valley_depth -= 1
+                    altitude += 1
+                elif letter_of_valley == 'D':
+                    valley_depth += 1
+                    altitude -= 1
+                if valley_depth == 0:
+                    valley_count += 1
+                    i += j - i  # advance forward the outer loop
+                    j = len(s)  # breaks the inner loop
+                else:
+                    j += 1
+        else:
+            i += 1
     return valley_count
