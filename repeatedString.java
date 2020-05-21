fea999e9b46b2d0791aefe22136b554a227d8616
= <zainr7989@gmail.com>
2020-05-21 17:24:41 -0400

    Init solution to 'repeated string'.

---
+++ repeatedString.java
@@ -0,0 +1,29 @@
+/*
+ * Hacker Rank Problem found here:
+ * https://www.hackerrank.com/challenges/repeated-string/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+ */
+ import java.io.*;
+ import java.math.*;
+ import java.security.*;
+ import java.text.*;
+ import java.util.*;
+ import java.util.concurrent.*;
+ import java.util.regex.*;
+
+ public class Solution {
+     //-----------------------------------------------------
+     // pre: String s, and long n
+     // post: return the number of times "a" appears in
+     //       first n letters of s, repeated over and over
+     //------------------------------------------------------
+     public static long repeatedString(String s, long n){
+
+     }
+     //--------------------------------------------------
+     // pre: None
+     // post: Solution to the "Repeated String" problem.
+     //--------------------------------------------------
+     public static void main(String[] args){
+
+     }
+ }
