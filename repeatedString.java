60bcb24aeec1acdbde3fd1271f3ba8b49f5e8b83
= <zainr7989@gmail.com>
2020-05-21 17:41:26 -0400

    Implement main method to demo test input.

--- repeatedString.java
+++ repeatedString.java
@@ -18,31 +18,35 @@
      //------------------------------------------------------
      public static long repeatedString(String s, long n){
         // count the number of times "a" appears in s
-            long numAsInOne = 0;
-            for(int i = 0; i < s.length(); i++){
-                if(s.charAt(i) == 'a'){
-                    numAsInOne += 1;
-                }
+        long numAsInOne = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'a'){
+                numAsInOne += 1;
             }
+        }
         // obtain roughly the number of "a" appearances in n letters
-            long total = numAsInOne * (n / s.length());
+        long total = numAsInOne * (n / s.length());
         // store the remainder, the letters of s that partially fit in n letters
-            long remS = n % s.length();
+        long remS = n % s.length();
         // count number of times "a" appears in first r letters of s
-            long numAsInRest = 0;
-            for(int i = 0; i < remS; i++){
-                if (s.charAt(i) == 'a'){
-                    numAsInRest += 1;
-                }
+        long numAsInRest = 0;
+        for(int i = 0; i < remS; i++){
+            if (s.charAt(i) == 'a'){
+                numAsInRest += 1;
             }
+        }
         // return the total
-            return (total + numAsInRest);
+        return (total + numAsInRest);
     }
      //--------------------------------------------------
      // pre: None
      // post: Solution to the "Repeated String" problem.
      //--------------------------------------------------
      public static void main(String[] args){
-
+         // execute repeatedString on a reasonable test input
+         String s = "aba";
+         long n = 10;
+         long As = repeatedString(s, n);
+         System.out.println("The correct ouput for the above is: " + As);
      }
  }
