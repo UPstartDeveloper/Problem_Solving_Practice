0f08f379825227b5de5079b31e8ef8e9dd653b4e
= <zainr7989@gmail.com>
2020-05-21 17:38:14 -0400

    Implement repeatedString method.

--- repeatedString.java
+++ repeatedString.java
@@ -17,8 +17,27 @@
      //       first n letters of s, repeated over and over
      //------------------------------------------------------
      public static long repeatedString(String s, long n){
-
-     }
+        // count the number of times "a" appears in s
+            long numAsInOne = 0;
+            for(int i = 0; i < s.length(); i++){
+                if(s.charAt(i) == 'a'){
+                    numAsInOne += 1;
+                }
+            }
+        // obtain roughly the number of "a" appearances in n letters
+            long total = numAsInOne * (n / s.length());
+        // store the remainder, the letters of s that partially fit in n letters
+            long remS = n % s.length();
+        // count number of times "a" appears in first r letters of s
+            long numAsInRest = 0;
+            for(int i = 0; i < remS; i++){
+                if (s.charAt(i) == 'a'){
+                    numAsInRest += 1;
+                }
+            }
+        // return the total
+            return (total + numAsInRest);
+    }
      //--------------------------------------------------
      // pre: None
      // post: Solution to the "Repeated String" problem.
