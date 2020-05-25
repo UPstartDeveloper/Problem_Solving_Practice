33899737d1596823b462d69a81843f6965867c52
= <zainr7989@gmail.com>
2020-05-25 10:58:07 -0400

    Try different approach for NewYorkQueue.

--- NewYearQueue.java
+++ NewYearQueue.java
@@ -13,26 +13,37 @@
   *        b. otherwise if the element is greather than its index by at least 2,
                 increment the number of swaps by 1 less than the difference
   *
+  * Observations:
+  * it is possible for an element to go ahead of another element that is much smaller than it;
+  * HOWEVER any one element will always be found within 2 positions of where it should be (index + 1)
+  * it is assumed that each element only moves in one direction, from where their original index was
+  * 
+  * Idea 2: using the differences between adjacent elements
+  *     A: iterate over the array
+  *         a. deciding it's too chaotic is the same
+            b. otherwise, the way we calculate the number of bribes is the following:
+                whenever two elements are out of place, we increment bribesCount by their difference
   */
 
  public class NewYearQueue{
     static void minimumBribes(int[] q) {
-        int swaps = 0;
+        int bribes = 0;
         boolean isChaotic = false;
-        for (int i = 0; i < q.length; i++){
-            int element = q[i];
-            int difference = element - i;
-            // the element is not impossible to exist at that space
+        for (int i = 0; i < q.length - 1; i++){
+            int elementBefore = q[i], elementAfter = q[i + 1];
+            int difference = elementBefore - i;
+            // the element cannot possibly to exist at that space
             if (difference > 3){
                 isChaotic = true;
-            } else if (difference >= 2){ // contribute number of swaps
-                swaps += difference - 1;
+            // increment number of bribes when elements out of order
+            } else if (elementBefore > elementAfter){
+                bribes += elementBefore - elementAfter;
             }
         }
         if (isChaotic == true){
             System.out.println("Too chaotic");
         } else {
-            System.out.println(swaps);
+            System.out.println(bribes);
         }
     }
 
