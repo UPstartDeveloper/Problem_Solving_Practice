62a038a275282a33d8d57dbf8cc9e3b4d42520ad
= <zainr7989@gmail.com>
2020-06-14 19:49:38 -0400

    Init solution for Flipping Bits.

---
+++ BitFlipper.java
@@ -0,0 +1,17 @@
+/*
+ * The following solution is provided for the Hacker Rank problem at this link:
+ * http://hr.gs/bddefe 
+ */
+
+public class BitFlipper {
+
+    // Complete the flippingBits function below.
+    static long flippingBits(long n) {
+
+        return n;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
