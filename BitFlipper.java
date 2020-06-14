320846661c5389f8a041603434dd6bc73768f710
= <zainr7989@gmail.com>
2020-06-14 19:52:30 -0400

    Add pseudocode for flipping bits.

--- BitFlipper.java
+++ BitFlipper.java
@@ -6,9 +6,18 @@
 public class BitFlipper {
 
     // Complete the flippingBits function below.
+    /*
+     * Pre: long n is an unsigned decimal integer
+     * Post: long is an unsigned decimal integer, and is found by
+     *       "flipping" all the bits in the binary representation 
+     *      of the input
+     */
     static long flippingBits(long n) {
-
+        // convert n to binary
+        // flip bits
+        // return the equivalent of the flipped binary number
         return n;
+        
     }
 
     public static void main(String[] args) {
