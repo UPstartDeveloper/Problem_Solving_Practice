b37e69f8e379fcf7a422c7e4d0779f38cb1aae7c
= <zainr7989@gmail.com>
2020-08-02 10:48:47 -0400

    Improve min_swaps solution.

--- min_swaps.py
+++ min_swaps.py
@@ -1,5 +1,28 @@
 from typing import List
 
+# Problem Description on Hacker Rank: http://hr.gs/3apgt 
+
+"""
+Variable-Value Table:
+
+num_swaps: 5
+
+should_be_index: 4
+
+minimum: 5
+
+min_index: 5
+
+      0  1  2  3  4  5  6
+arr: [7, 1, 3, 2, 4, 5, 6]
+     [1, 7, 3, 2, 4, 5, 6]
+     [1, 2, 3, 7, 4, 5, 6]
+     [1, 2, 3, 4, 7, 5, 6]
+     [1, 2, 3, 4, 5, 7, 6]
+     [1, 2, 3, 4, 5, 6, 7]
+
+"""
+
 def min_swaps(arr: List[int]): 
     """Return the minimum number of swaps needed to sort a list in ascending
        order. 
@@ -23,7 +46,6 @@ def min_swaps(arr: List[int]):
         # track what the value and index of lowest is
         minimum = min(arr[should_be_index:])
         min_index = arr.index(minimum)
-        print(minimum, min_index)
         # if not where it should be, swap the indices
         if min_index != should_be_index:
             swap(min_index, should_be_index)
@@ -31,11 +53,11 @@ def min_swaps(arr: List[int]):
             num_swaps += 1
         # on next iteration, the lowest should go in the next index
         should_be_index += 1
-        print(arr)
     return num_swaps
 
 
 
 if __name__ == '__main__':
-    print(min_swaps([1, 3, 5, 2, 4, 6, 7]))
+    arr = [7, 1, 3, 2, 4, 5, 6]
+    print(min_swaps(arr))
     
