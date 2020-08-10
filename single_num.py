fb24bb64ad277122ec138bd5de5544f79ef635f0
= <zainr7989@gmail.com>
2020-08-10 16:07:10 -0400

    add OOP solution for single number II.

--- single_num.py
+++ single_num.py
@@ -1,3 +1,4 @@
+"""
 def find_max_power_of_2(max_num):
     exponent = 0
     power = 2**exponent
@@ -15,9 +16,9 @@ def is_in_range(num, target_appearances, rest_appearances):
     return (range_value == num)
 
 def align_nums(nums, bits_needed):
-    for index, num in enumerate(nums):
+    for index, num in enumerate(nums):  # n iterations
         # see difference in lengthof num, and needed num of bits
-        diff = bits_needed - len(num)
+        diff = bits_needed - len(num)  # O(1)
         # make up the length difference
         if diff > 0:
             # convert to list, to be able to insert
@@ -34,12 +35,12 @@ def align_nums(nums, bits_needed):
 
 def single_number(nums):
     # find how long all the binary numbers need to be, n
-    max_num = max(nums)
-    bits_needed = find_max_power_of_2(max_num)
+    max_num = max(nums)  # O(n)
+    bits_needed = find_max_power_of_2(max_num)  # O(n)
     # convert all the nums to their binary equivalents
-    nums = [bin(num)[2:] for num in nums]
+    nums = [bin(num)[2:] for num in nums]  # O(n)
     # make sure all the nums are the same length
-    nums = align_nums(nums, bits_needed)
+    nums = align_nums(nums, bits_needed)  
     print(nums)
     # create the final binary number we need to decode
     final_binary = list()
@@ -65,6 +66,38 @@ def single_number(nums):
     # decode the binary number into base 10
     target_num = int(binary_str, 2) 
     return target_num
+
+"""
+
+
+# This model solution is from Prof. Narayan, at Cal State Monterey Bay
+
+class Trinary:
+    def __init__(self):
+        self.bits = [0 for i in range(32)]
+        print(f'Initialized Tri: {self.bits}')
+    def xor(self, num):
+        for i in range(32):
+            bit = 1 << i
+            if num & bit:
+                print(f'Num: {num} and mask: {bit}')
+                # when a bit occurs any multiple of three times, it will roll back to 0
+                self.bits[i] = self.bits[i] + 1 if self.bits[i] + 1 <= 2 else 0
+                print(f'Bits changed => {self.bits}')
+    def value(self):
+        num = 0
+        for i in range(32):
+            if self.bits[i] == 1:
+                num |= 1 << i
+        return num
+
+def single_number(nums):
+    tri = Trinary()
+    for num in nums:
+        tri.xor(num)
+    return tri.value()
+  
+
 
 if __name__ == '__main__':
     nums = [3, 3, 3, 10]
