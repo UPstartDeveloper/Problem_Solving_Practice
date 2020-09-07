0d518e4c5c0c0af053a541d18640c30a092c8b50
= <zainr7989@gmail.com>
2020-09-07 09:02:58 -0400

    Improve solution for simplify_path.

--- simplify_path.py
+++ simplify_path.py
@@ -1,3 +1,4 @@
+"""
 def simplifyPath(path):
     # split by /
     split_by_slash = path.split('/')
@@ -21,10 +22,45 @@ def simplifyPath(path):
         if index < len(canon) - 1:
             path += SLASH
     return path
+"""
+
+def simplifyPath(path):
+  # place chars for simplified version in a stack
+  canon_path = list(['/'])
+  # iterate over the path
+  index = 1
+  while index < len(path):
+    # get the symbol at this index
+    char = path[index]
+    # directories to remove
+    if char == '.':
+      if len(canon_path) > 0:
+        canon_path.pop()
+    elif (char == '/' and canon_path[-1] == '/'):
+        pass
+    # characters to add
+    else:
+      canon_path.append(char)
+    # move on to the next char
+    index += 1
+  # convert list to a string - remove the trailing / if needed
+  if len(canon_path) > 1:
+    return ''.join(canon_path[:-1])
+  else:
+    return ''.join(canon_path)
+    
+  
+"""
+path
+/home//foo/
+
+canon_path
+/home/
+"""
    
  
 if __name__ == '__main__':
-    testPath = "/a//b////c/d//././/.."
+    testPath = "/a/./b/../../c/"
     testResult = simplifyPath(testPath)
     print(testResult)
 
