5722340b68f964c27a5d4be57f31afdac77f31ec
= <zainr7989@gmail.com>
2020-09-07 09:18:42 -0400

    Optimize simplify path solution.

--- simplify_path.py
+++ simplify_path.py
@@ -1,73 +1,51 @@
-"""
-def simplifyPath(path):
-    # split by /
-    split_by_slash = path.split('/')
-    # init canonical list
-    canon = list()
-    # iterate and add important tokens
-    for token in split_by_slash:
-	    if len(token) > 0:
-	        # go back up a level
-	        if len(canon) > 0 and token == '..':
-                canon.pop(-1)
-	        # go down a level
-            else:  # we have a new directory to go down to
-                canon.append(token)     
-    # join the paths
-    SLASH = '/'
-    path = SLASH
-    for index, token in enumerate(canon):
-        path += token
-        # add slashes for all token other than the last
-        if index < len(canon) - 1:
-            path += SLASH
-    return path
-"""
+from collections import deque
 
 def simplifyPath(path):
-  # place chars for simplified version in a stack
-  canon_path = list(['/'])
-  # iterate over the path
-  index = 1
-  while index < len(path):
-    # get the symbol at this index
-    char = path[index]
-    # directories to remove
-    if char == '.':
-      if len(canon_path) > 0:
-        canon_path.pop()
-    elif (char == '/' and canon_path[-1] == '/'):
-        pass
-    # characters to add
-    else:
-      canon_path.append(char)
-    # move on to the next char
-    index += 1
-  # convert list to a string - remove the trailing / if needed
-  if len(canon_path) > 1:
-    return ''.join(canon_path[:-1])
-  else:
-    return ''.join(canon_path)
+    """Converts a UNIX-style file path to its canonical version.
+
+       Parameters:
+       path(str): a file path to a certain directory, may been seen on 
+                  Linux or Mac OSX.
+       
+       Returns: str: the canonical form of path
     
-  
-"""
-path
-/home//foo/
+    Assumptions:
+    - the path is mutable 
+    - the path always begins with a leading slash
 
-canon_path
-/home/
-"""
-   
- 
+    Complexity:
+    O(n) time, because we iterate over all characters in the path string
+    O(1) space, because we reuse all the letters in string, and add no extra
+    """
+    # place chars for simplified version in a stack
+    path = deque(path)
+    canon_path = list()
+    # iterate over the path
+    while len(path) > 0:
+        # get the symbol at the start
+        char = path.popleft()
+        # flag for when to stop if/else logic
+        check_again = True
+        # directories to remove
+        if char == '.':
+            if len(canon_path) > 0:
+                canon_path.pop()
+                check_again = False
+        # characters to skip
+        elif char == '/':
+            if len(canon_path) > 0 and canon_path[-1] == '/':
+                check_again = False
+        # characters to add
+        if check_again == True:
+            canon_path.append(char)
+    # convert list to a string - remove the trailing / if needed
+    if len(canon_path) > 1:
+        return ''.join(canon_path[:-1])
+    else:
+        return ''.join(canon_path)
+    
+
 if __name__ == '__main__':
     testPath = "/a/./b/../../c/"
     testResult = simplifyPath(testPath)
     print(testResult)
-
-
-"""
-split_by_slash                        |  canon            |  path
-['a', 'b', 'c', 'd', '.', '.', '..']  | ['a', 'b', 'c',]  | '/a/b/c
-
-
-"""
