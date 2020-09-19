adc6f0d711836029fedf5740b54a600589316cb6
= <zainr7989@gmail.com>
2020-09-18 22:53:50 -0400

    Implement brute force solution for array quadruple problem.

--- array_quadruple.py
+++ array_quadruple.py
@@ -1,11 +1,20 @@
-def array_quadruple(nums, target_sum):
-    for i in range(len(nums)):
-        i_num = nums[i]
-        for j in range(len(nums)):
-            if j != i:
-                j_num = nums[j]
-            for k in range(len(nums)):
-                for l in range(len(nums)):
+def find_array_quadruplet(arr, s):
+    '''Returns a quadtuple of elements in arr, that sum to s, in sorted order.'''
+    solution = []
+    arr.sort()
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if i != j:
+                for k in range(len(arr)):
+                    if k != j and k != i:
+                        for l in range(len(arr)):
+                            if l != i and l != k and l != j:
+                                p1, p2, p3, p4 = (arr[i], arr[j], arr[k], arr[l])
+                            if (p1 + p2 + p3 + p4) == s:
+                                return sorted([p1, p2, p3, p4])
+    return solution 
+
+
 
 
 if __name__ == "__main__":
