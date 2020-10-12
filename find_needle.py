b96f37d938ae0e190639201597b169ed95b4b350
= <zainr7989@gmail.com>
2020-10-12 11:46:39 -0400

    Add alternate solution for strStr.

--- find_needle.py
+++ find_needle.py
@@ -1,9 +1,10 @@
+"""
 def set_letters(haystack, needle, needle_index, haystack_index):
     needle_letter, haystack_letter = (
     needle[needle_index], haystack[haystack_index]
     )
     return needle_letter, haystack_letter
-"""
+
 def strStr(haystack, needle):
     # init a variable tracking the index of where the match begins
     match_start = 0
@@ -32,6 +33,7 @@ def strStr(haystack, needle):
     # needle not found
     return -1
 """
+"""
 # This is Abhishek Kulkarni's solution, I have pasted it here to compare
 # with the one I have above
 def strStr(haystack: str, needle: str):
@@ -42,10 +44,57 @@ def strStr(haystack: str, needle: str):
         if haystack[i:i+len(needle)] == needle:
             return i
     return -1
+"""
+"""
+def strStr(haystack, needle):
+    # early exit: needle is empty string
+    if needle == '':
+        return 0
+    # init the match at -1
+    # match = -1
+    # find where a letter of hs, matches the first letter of needle
+    hs_idx = 0
+    while hs_idx < len(haystack):
+        n_idx = 0
+        # if it does
+        if haystack[hs_idx] == needle[n_idx]:
+            # copy over the hs_index into another variable
+            h_match = hs_idx
+            # iterate hs using the hs_index, and needle using n_idx
+            while n_idx < len(needle) and h_match < len(haystack):
+                if needle[n_idx] == haystack[h_match]:
+                    n_idx += 1
+                    h_match += 1
+                else:
+                    break
+            # if hit the end of n_idx before end of hs, then return match_start
+            if n_idx == len(needle):
+                return hs_idx
+        # reset match_start to -1
+        # increment hs_index
+        hs_idx += 1
+    # return match_start
+    return -1
+    """
+
+def strStr(haystack, needle):
+    # early exit
+    if needle == '':
+        return 0
+    # iterate backwards through the haystack
+    for hs_idx in range(len(haystack) - len(needle), 0, -1):  # h - n iteration
+        # compare the substring of haystack to needle
+        if haystack[hs_idx:hs_idx+len(needle)] == needle:  # n iterations
+            return hs_idx
+    # match not found
+    return -1
+
+    # Time: linear - O(n(h - n)) --> O(hn)
+    # Space: O(n)
 
 
 if __name__ == '__main__':
-    print(strStr("aaaab", "b"))
+    print(strStr("aaaaaaaaaaa", "aaaaaaaaaab"))
 """
      0   1   2  3  4 
      h   e   l  o  o
