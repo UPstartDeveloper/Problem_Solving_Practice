cd9fcc61d6d93232137301b95b2d9d43475f6fcb
= <zainr7989@gmail.com>
2020-11-25 08:28:59 -0500

    Fix scoping errors in permutations solution.

--- permutations.py
+++ permutations.py
@@ -1,61 +1,46 @@
 import numpy as np 
-
-def get_permutations(input_list):
-    # init the empty output array
-    global output 
+class Solution:
     output = list()
-    def get_remaining_nums(nums_left, index):
-        """# calculate the amount of nums needed
-        nums_required = len(input_list) - len(permutation)
-        nums_taken, index = [], 0
-        # begin searching for numbers
-        while len(nums_taken) < nums_required:
-            num = input_list[index]
-            # decide to add the number or not
-            if num not in permutation:
-              nums_taken.append(num)
-            # iterate the index
-            index = (index + 1) % len(input_list)
-        return nums_taken"""
-        # make a deep copy of the array 
-        # (so the changes made to one don't affect the other)
-        remaining_nums = np.array(nums_left).copy()
-        remaining_nums = list(remaining_nums)
-        # remove the num just added, then return the rest
-        remaining_nums.pop(index)
-        return remaining_nums
-    def generate_permutations(nums_left, permutation):
-        # base case
-        if len(permutation) == len(input_list):
-            # add the num to the permutation
-            # permutation.append(nums_left.pop())
-            # append to the output
-            print(f'appended {permutation} to the output: {output}')
-            output.append(permutation)
-            return
-        # Recursive Case:
-        else:  # permutation is incomplete
-            # for each of the remaining numbers
-            for index in range(len(nums_left)):
-                print(f'At index {index} in array: {nums_left}')
-                num = nums_left[index]
-                # add it to the permuation so far
-                permutation.append(num)
-                print(f'Permutation: {permutation}')
-                # return the next recursive call (w/o nums already added)
-                # remaining_nums = get_remaining_nums(nums_left, permutation)
-                remaining_nums = get_remaining_nums(nums_left, index)
-                generate_permutations(remaining_nums, permutation)
-                permutation.pop()
-            return 
-    # init the perm to an empty array
-    perm = list()
-    # find all permutations
-    generate_permutations(input_list, perm)
-    return output
-  
+
+    def get_permutations(self, input_list):
+        # init the empty output array
+        def get_remaining_nums(nums_left, index):
+            # make a deep copy of the array 
+            # (so the changes made to one don't affect the other)
+            remaining_nums = np.array(nums_left).copy()
+            remaining_nums = list(remaining_nums)
+            # remove the num just added, then return the rest
+            remaining_nums.pop(index)
+            return remaining_nums
+        def generate_permutations(nums_left, permutation):
+            # base case
+            if len(permutation) == len(input_list):
+                # add the num to the permutation
+                # permutation.append(nums_left.pop())
+                # append to the output
+                next_perm = list(np.array(permutation).copy())
+                self.output.append(next_perm)
+                print(f'appended {permutation} to the output: {self.output}')
+                return
+            # Recursive Case:
+            else:  # permutation is incomplete
+                # for each of the remaining numbers
+                for index in range(len(nums_left)):
+                    num = nums_left[index]
+                    # add it to the permuation so far
+                    permutation.append(num)
+                    # return the next recursive call (w/o nums already added)
+                    remaining_nums = get_remaining_nums(nums_left, index)
+                    generate_permutations(remaining_nums, permutation)
+                    permutation.pop()
+        # init the perm to an empty array
+        perm = list()
+        # find all permutations
+        generate_permutations(input_list, perm)
+        return self.output
     
-print(get_permutations([1,2,3]))
+sol = Solution()
+print(sol.get_permutations([1,2,3]))
 
 """
 init the output = [
