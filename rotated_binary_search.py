0a10ce64f5e0d5632f465ba137e37a87c4e63024
= <zainr7989@gmail.com>
2020-12-10 11:28:23 -0500

    Solve Shifted Array search.

--- rotated_binary_search.py
+++ rotated_binary_search.py
@@ -1,7 +1,70 @@
+# Logarithmic Time, constant space solution
+def shifted_arr_search(shiftArr, num):
+  
+    def find_pivot():
+        '''Find the place where the arr splits into two sorted subranges'''
+        # init the low and hi pt'ers
+        lo, mid, hi = 0, 0, len(shiftArr) - 1
+        # find the pivot using BS
+        while lo <= hi:
+            # calculate the middle
+            mid = lo + ((hi - lo) // 2)
+            # judge if the middle is the pivot exactly
+            if mid == 0 or shiftArr[mid] < shiftArr[mid - 1]:
+                return mid
+            # if the mid is greater than the first element
+            elif shiftArr[0] < shiftArr[mid]:
+                # go search the right half to find the pivot
+                lo = mid + 1
+            # if the mid is less than the first
+            elif shiftArr[0] > shiftArr[mid]:
+                # go search the left half to find the pivot
+                hi = mid - 1
+        return mid
+        
+    def bin_search(lo, hi, target):
+        '''Executes Binary Search recursively'''
+        while lo <= hi:
+            # calculate the middle
+            mid = (lo + hi) // 2
+            middle = shiftArr[mid]
+            # compare to the target
+            if middle == target:
+                return mid
+            # search the left side
+            elif middle > target:
+                hi = mid - 1
+            else:  # search the right side
+                lo = mid + 1
+        # not found case (when lo > hi)
+        return -1
+  
+    # find the pivot
+    pivot = find_pivot()
+    # find answers for the left and right sides
+    if len(shiftArr) == 1:
+        return pivot
+    # otherwise, 
+    # search the left side of the pivot
+    left_side_ans = bin_search(0, pivot - 1, num)
+    # search everything else
+    right_side_ans = bin_search(pivot, len(shiftArr) - 1, num)
+    # return the index
+    if left_side_ans == right_side_ans == -1:  # not found
+        return -1 
+    elif left_side_ans == -1:
+        return right_side_ans
+    else:  # not found on the right, but on the left
+        return left_side_ans
+
+
+
+"""
+Scratch Work
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         # pivot > 0
-        """
+        '''
         brute force = linear search
         All values of nums are unique.
         
@@ -65,7 +128,7 @@ class Solution:
         see which of those values is closer to the target
         go search that half of the array
             
-        """
+        '''
         def rotated_binary_search(low, high):
             # go to the middle
             middle_index = ((low + high) // 2)
@@ -98,17 +161,19 @@ class Solution:
                         # search the right side
                         return rotated_binary_search(middle_index + 1, high)
                 # just search the right side by deafult
-                """elif middle_index == 0 and middle_index < len(nums):
+                elif middle_index == 0 and middle_index < len(nums):
                     return rotated_binary_search(middle_index + 1, high)
                 # just search the left by default
                 elif middle_index == len(nums) - 1 and middle_index > 0:
-                    return rotated_binary_search(low, middle_index - 1)"""
-                print('not in any if statements!')
-                print(f'Middle: {mid}, low: {low}, and high: {high}')
-                return -1
+                    return rotated_binary_search(low, middle_index - 1)
+                    """
+                # print('not in any if statements!')
+                # print(f'Middle: {mid}, low: {low}, and high: {high}')
+                # return -1
         # --------------------------------------------------------------------
-        return rotated_binary_search(0, len(nums) - 1)               
-"""
+        # return rotated_binary_search(0, len(nums) - 1)    
+"""           
+
 nums = [4,5,6,7,0,1,2], target = 0
 
 SF 1
