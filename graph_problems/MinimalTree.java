a5741c803dd384a628892dae3ccafdde8c8f9544
= <zainr7989@gmail.com>
2020-12-14 13:10:46 -0500

    Init solution for MinimalTree problem.

---
+++ graph_problems/MinimalTree.java
@@ -0,0 +1,47 @@
+package graph_problems;
+
+public class MinimalTree {
+    /* Problem: Given a sorted (increasing order) array with unique integer elements, 
+     * write an algorithm to create a binary search tree with minimal height.
+     * 
+     * [-6, 0, 3, 5, 23, 54]
+     * Return the root of the new tree.
+     */
+
+    public class Node {
+        // declare class attributes
+        int key;
+        Node left;
+        Node right;
+
+        // constructor - children are initialized to null values
+        public Node(int key) {
+            this.key = key;
+            this.left = null;
+            this.right = null;
+        }
+
+        public void setLeft(Node leftChild) {
+            this.left = leftChild;
+        }
+
+        public void setRight(Node rightChild) {
+            this.right = rightChild;
+        }
+    }
+
+    // declare the class attributes on the Tree
+    Node root;
+    int[] numbers;
+
+    public MinimalTree(int[] numbers) {
+        this.root = null;
+        this.numbers = numbers;
+    }
+
+    // solution to the problem
+    public Node populateTree() {
+        
+        return this.root;
+    }
+}
