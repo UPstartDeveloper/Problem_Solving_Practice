a9ac1104edbcf1a731aa755662caa970234fce10
= <zainr7989@gmail.com>
2020-12-20 12:37:04 -0500

    Add test case and time and space analysis for remove duplicates.

--- linked_list_problems/remove_dupes.py
+++ linked_list_problems/remove_dupes.py
@@ -7,13 +7,23 @@ class LinkedList:
     def __init__(self, head):
         self.head = head
 
-    def delete(prev, node):
+    def delete(self, prev, node):
+        # "delete" the node that's a dupe
         prev.next = node.next
+        # move the iterator node ahead from the deleted node's place
+        node = prev.next
+        return prev, node
 
     def __str__(self):
-        pass
+        items = list()
+        n = self.head
+        while n is not None:
+            items.append(n.data)
+            n = n.next
+        return str(items)
 
     def remove_duplicates(self):
+        '''Linear time and space'''
         # init a set
         unique_elems = set()
         # iterate over the list
@@ -22,8 +32,10 @@ class LinkedList:
             # check if this Node is a duplicate
             if node.data in unique_elems:
                 # delete the node from the list
-                self.delete(prev, node)
+                prev, node = self.delete(prev, node)
             else:  # node does not have a duplicate value
+                # add the value to the set
+                unique_elems.add(node.data)
                 # move both nodes ahead
                 prev = node
                 node = node.next
@@ -31,4 +43,22 @@ class LinkedList:
 
 
 if __name__ == "__main__":
-    pass
+    # 1 --> -1 --> 6 --> 5 --> 7 -->
+    """
+    unique = {1, -1, 6, 5, 7}
+    p = 7
+    n = None
+    """
+    # Test Case 1:
+    items = [1, -1, 6, 5, 1, 5, 7]
+    # form the list
+    head = Node(items[0])
+    ll = LinkedList(head)
+    node = ll.head
+    for index in range(1, len(items)):
+        new_node = Node(items[index])
+        node.next = new_node
+        node = new_node
+    # test the output of the function
+    ll.remove_duplicates()
+    print(f'Items: {ll}')
