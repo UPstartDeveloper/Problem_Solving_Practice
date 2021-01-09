7b5da2165e418208753456723a26ba21f8cda00a
= <zainr7989@gmail.com>
2021-01-09 18:14:23 -0500

    Add binary search solution for Sorted Search No Size.

--- sorting_problems/sorted_search_no_size.py
+++ sorting_problems/sorted_search_no_size.py
@@ -1,3 +1,6 @@
+import math
+
+
 """
 
 Sorted Search, No Size: 
@@ -24,6 +27,8 @@ Observations
 - there can be duplicates (but we can return any single index)
 - first index in Listy is always 0
 - last index is always n - 1 (n = # elements)
+- if List.elementAt(i) > x, then you can return -1 if you've already 
+  seen everything to the left of i (linear search)
 
 Intuition:
 - use binary search
@@ -42,9 +47,28 @@ Brainstorming:
     - start out at index 0
     - check each successive index until we find the element, or encounter -1
 
-1. Modified Binary Search
+    n <,>,= (log(n)) + (n / 2)
+
+1. Modified Binary Search - "runners"
     - if there's no size attribute
         - do a linear search to find the size of the list (obviously slow)
+
+    - have two runners
+    - first moves one by one
+    - second moves 2 at a time
+    - purpose of this is that after n/2 iterations -> 1 is halfway through the list
+                                                   -> 2 is either at the end, or just beyond
+    - from there, you have the low, middle, and high to do binary search
+    O((n/2) + log(n))
+
+     0  1  2  3  4  5  6  7  8
+    [1, 3, 3, 3, 5, 7, 8, 9, 9]
+     ^  ^
+        ^     ^
+           ^         ^
+              ^           ^
+                 ^              ^
+
 
 
 Test Inputs:
@@ -76,24 +100,72 @@ class Listy:
             return self.nums[i]
         else: 
             return -1
+    
+    def length(self):
+        '''Return the length of self.nums. No built-ins!
+         0  1  2   3
+        [5, 3, 3] -1
+        '''
+        # A: init 2 pointers
+        slow, fast = 0, 1
+        # B: find where the second "falls off" the listy
+        while (self.element_at(fast) != -1) and (self.element_at(fast + 1) != -1):
+            #           T, F T, F                           T, T, F, F
+            # C: move both ahead at different speeds
+            slow += 1
+            fast += 2
+        # D: move the fast back until it's in bounds
+        while self.element_at(fast) == -1:
+            fast -= 1
+        # E: return the length
+        return fast + 1
 
 
-def sorted_arr_no_size(listy, x):
-    '''Performs linear search search for x in listy, and return the index/-1.'''
-    # A: init index at 0
-    index = 0
-    # B: iterate through indices of the Listy
-    element = listy.element_at(index)
-    while element != -1 and element < x:
-        # increment the index and get the next element
-        index += 1
+class Solution:
+
+    def sorted_arr_no_size_naive(listy, x):
+        '''Performs linear search search for x in listy.'''
+        # A: init index at 0
+        index = 0
+        # B: iterate through indices of the Listy
         element = listy.element_at(index)
-    # C: return the answer (assume that x was found)
-    answer = index
-    # if x was not found, then actually return -1
-    if element != x:
-        answer = -1
-    return answer
+        while element != -1 and element < x:
+            # increment the index and get the next element
+            index += 1
+            element = listy.element_at(index)
+        # C: return the answer (assume that x was found)
+        answer = index
+        # if x was not found, then actually return -1
+        if element != x:
+            answer = -1
+        return answer
+
+    def sorted_arr_no_size_binary_search(listy, x):
+        '''Performs modified binary search for the index of x.'''
+        def binary_search(low, high, target):
+            # A: compare the elem at middle with target (x)
+            while low <= high:
+                # B: init the middle index
+                middle = (low + high) // 2
+                mid_elem = listy.element_at(middle)
+                # if match -> return the middle index
+                if mid_elem == target:
+                    return middle
+                # if middle > target -> search the lower half
+                elif mid_elem > target:
+                    high = middle - 1
+                # if middle < target -> search the greater half
+                elif mid_elem < target:
+                    low = middle + 1
+            # C: return -1 if it can't be found
+            return -1
+        # A: find out the length of listy in n / 2 iterations
+        high_idx = listy.length() - 1
+        # B: binary search from there
+        return binary_search(0, high_idx, x)
+
+
+
 
 
 """
@@ -105,6 +177,16 @@ i   e   answer = 2
 0   1
 1   3
 
+ 0  1  2  3  4  5  6  7  8  9
+[1, 3, 3, 3, 5, 7, 8, 9, 9, 10]
+             s               f
+ l           m               h
+ l  m     h  
+
+high_index = 9, 3
+low = 0, 0
+mid = 4, 1
+x = 3
 """
 
 
@@ -117,5 +199,6 @@ if __name__ == "__main__":
     ]
     array = Listy([1, 3, 3, 3, 5, 7, 8, 9, 9])
     # check if we get the right answer for each text input
+    sol = Solution
     for x, ans in inputs:
-        assert sorted_arr_no_size(array, x) == ans
+        assert sol.sorted_arr_no_size_binary_search(array, x) == ans
