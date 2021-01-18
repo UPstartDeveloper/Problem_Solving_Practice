dc9e1f4065cfd082f47ce0ecf5786c99fd22d0c1
= <zainr7989@gmail.com>
2021-01-17 19:59:55 -0500

    Init iterative in-order DFS method.

---
+++ Drills/tree.py
@@ -0,0 +1,24 @@
+class Tree:
+    def __init__(self, root):
+        self.root = root
+
+    def in_order_traversal(self):
+        
+        def _dfs_iterative(self):
+            # push the root onto the stack
+            # while stack !empty:  
+                # peek at the top node
+                # iteratively add the unvisited left children
+                # visit the top of the stack - add to the visited set
+                # push the first right child onto the stack, if unvisited
+            # return the result
+            pass
+        return _dfs_iterative()
+
+
+class TreeNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
