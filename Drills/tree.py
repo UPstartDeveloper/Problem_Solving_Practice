816b5a1bf0d67b1f65d118c7a6ef844d8f97e4cc
= <zainr7989@gmail.com>
2021-02-16 18:28:57 -0500

    Drill in order DFS (iterative and recursive.

--- Drills/tree.py
+++ Drills/tree.py
@@ -2,18 +2,45 @@ class Tree:
     def __init__(self, root):
         self.root = root
 
-    def in_order_traversal(self):
+    def in_order_traversal(self, use_iteration=True):
+
+        def _dfs_recursive(node, visited):
+            if node.left is not None:
+                _dfs_recursive(node.left)
+            visited.add(node)
+            if node.right is not None:
+                _dfs_recursive(node.right)
+            return visited
         
-        def _dfs_iterative(self):
+        def _dfs_iterative():
+            # init collectionss
+            stack = list()
+            visited = set()
             # push the root onto the stack
-            # while stack !empty:  
-                # peek at the top node
-                # iteratively add the unvisited left children
-                # visit the top of the stack - add to the visited set
-                # push the first right child onto the stack, if unvisited
+            if self.root is not None:
+                stack.append(self.root)
+                # while stack !empty:  
+                while len(stack) > 0:
+                    # peek at the top node
+                    top = stack[-1]
+                    # iteratively add the unvisited left children
+                    while top is not None:
+                        if top.left is not None and top.left not in visited:
+                            stack.append(top.left)
+                        top = top.left
+                    # visit the top of the stack - add to the visited set
+                    if top is None and len(stack) > 0:
+                        top = stack.pop()
+                    visited.add(top)
+                    # push the first right child onto the stack, if unvisited
+                    if top.right is not None and top.right not in visited:
+                        stack.append(top.right)
             # return the result
-            pass
-        return _dfs_iterative()
+            return visited
+            
+        if use_iteration is True:
+            return _dfs_iterative()
+        return _dfs_recursive(self.root, set())
 
 
 class TreeNode:
