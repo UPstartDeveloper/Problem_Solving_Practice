9d4e90f724a42d8d0e940c52d879148698e54112
= <zainr7989@gmail.com>
2021-02-17 10:46:48 -0500

    Fix symmetric tree solution.

--- tree_problems/symmetric_tree.py
+++ tree_problems/symmetric_tree.py
@@ -1,11 +1,11 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    '''definition of a binary tree node'''
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
-    def isSymmetric(self, root: TreeNode) -> bool:
+    def isSymmetric(self, root: TreeNode, use_recursion=True) -> bool:
         """
         null values matter
         
@@ -69,40 +69,23 @@ class Solution:
         values = [None, 2, None, 3, None, 1, None, 2, None, 3, None ]
                             
         """  
-        def convert_to_array(root):
-            def in_order_traverse(node, visit):
-                if node.left is not None:
-                    in_order_traverse(node.left, visit)
-                else:
-                    visit(None)
-                # visit this node
-                visit(node.val)
-                if node.right is not None: 
-                    in_order_traverse(node.right, visit)
-                else:
-                    visit(None)
-            # init the array
-            values = list()
-            in_order_traverse(root, values.append)
-            return values
+        def compare_nodes(node1, node2):
+            """O(n) time, O(log n) space"""
+            # Base Case: we've gone beyond the leaf nodes
+            if node1 == node2 == None:
+                return True
+            # Base Case: nodes are not equal
+            elif (node1 is None) or (node2 is None) or (node1.val != node2.val):
+                return False
+            # Recursive Case: nodes are equal
+            elif node1.val == node2.val:
+                return (
+                    compare_nodes(node1.left, node2.right) and
+                    compare_nodes(node1.right, node2.left)
+                )
         
-        def is_valid(array) -> bool:
-            # init the two pointers at the middle
-            left, right = (len(array) - 1) // 2, (len(array) - 1) // 2
-            # "walk away" from the middle
-            while left > -1 and right < len(array):  # n / 2
-                left_elem = array[left]
-                right_elem = array[right]
-                # move on to the next step
-                if left_elem == right_elem:
-                    left -= 1
-                    right += 1
-                else:
-                    return False
-            return True
-            
-        # A: get all the values in an array (recursive DFS)
-        values = convert_to_array(root)
-        print(values)
-        # B: validate the array
-        return is_valid(values)
+        is_symmetric = True
+        if root is not None and use_recursion is True:
+            is_symmetric = compare_nodes(root.left, root.right)
+        return is_symmetric
+               
