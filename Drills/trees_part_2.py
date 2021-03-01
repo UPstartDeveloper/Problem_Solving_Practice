5ce1fa9f83e8ba08131327188a8eeae38eb05781
= <zainr7989@gmail.com>
2021-03-01 09:21:31 -0500

    Implement pre- and post-order DFS iteratively on binary tree.

--- Drills/trees_part_2.py
+++ Drills/trees_part_2.py
@@ -50,29 +50,78 @@ class BinaryTree:
 
     def dfs(self, use_iteration=True, mode='in-order') -> None:
 
-        def _dfs_iterative_in_order():
-            '''due to the use of a set - time is O(n), space is O(n)'''
-            # init stack
-            stack = list()
-            visited = set()
-            # push the first node
+        def _dfs_iterative_setup():
+            stack, visited = list(), set()
             if self.root is not None:
                 stack.append(self.root)
+            return stack, visited
+
+        def _dfs_iterative_in_order():
+            '''due to the use of a set - time is O(n), space is O(n)'''
+            # init collections - root is already on the stack if present
+            stack, visited = _dfs_iterative_setup()
             # traverse the tree
             while len(stack) > 0:
                 # go down as far left as possible from the top
                 top = stack[-1]
+                left_child = top.left
+                while left_child is not None and left_child not in visited:
+                    stack.append(left_child)
+                    left_child = left_child.next
+                # visit the top
+                top = stack.pop()
+                print(f"Visiting: {top.key}")
+                visited.add(top)
+                # push the first right child
+                if top.right is not None and top.right not in visited:
+                    stack.append(top.right)
+
+        def _dfs_iterative_pre_order():
+            '''due to the use of a set - time is O(n), space is O(n)'''
+            # init collections - root is already on the stack if present
+            stack, visited = _dfs_iterative_setup()
+            # traverse the tree
+            while len(stack) > 0:
+                # visit the top
+                top = stack.pop()
+                print(f"Visiting: {top.key}")
+                visited.add(top)
+                # go down as far left as possible from the top
                 while top.left is not None and top.left not in visited:
                     stack.append(top.left)
                     top = top.left
+                # push the first right child
+                if top.right is not None and top.right not in visited:
+                    stack.append(top.right)
+        
+        def _dfs_iterative_post_order():
+            '''due to the use of a set - time is O(n), space is O(n)'''
+            # init collections - root is already on the stack if present
+            stack, visited = _dfs_iterative_setup()
+            # traverse the tree
+            while len(stack) > 0:
+                top = stack[-1]
+                # go down as far left as possible from the top
+                left_child = top.left
+                while left_child and left_child not in visited:
+                    stack.append(left_child)
+                    # make sure to save a ref to the last left node pushed
+                    if left_child.left:
+                        left_child = left_child.left
+                    else:
+                        break
+                # push the first right child
+                right_child = top.right
+                if right_child and right_child not in visited:
+                    stack.append(right_child)
                 # visit the top
-                if len(stack) > 0:
+                if left_child and left_child not in visited:
+                    top = left_child
+                else:  # get already visited node, or root off the stack
                     top = stack.pop()
-                    print(f"Visiting: {top.key}")
+                if top not in visited:
+                    print(f"Visiting: {top.key}") 
                     visited.add(top)
-                    # push the first right child
-                    if top.right is not None and top.right not in visited:
-                        stack.append(top.right)
 
         def _dfs_recursive_in_order(node=self.root):
             if node is not None: 
