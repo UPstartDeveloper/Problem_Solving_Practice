87ecca1b376036c59e5148237548bf05fc2e668d
= <zainr7989@gmail.com>
2021-03-13 09:26:13 -0500

    Improve string_rotation solution.

--- array_str_problems/string_rotation.py
+++ array_str_problems/string_rotation.py
@@ -38,6 +38,23 @@ Approach:
         - make sure it's not equal, and isSubstring is True ---> TRue
         - any other case --False
 
+Code I used before:
+        # find the first letter of s1 in s2
+        first_letter_s1 = s1[0]
+        ndx_first_letter_s2 = s1.find(first_letter_s1)
+        # take the substring from there, till the end
+        substr_s2 = s2[ndx_first_letter_s2:]
+        # check if that != s1
+        if s1 != substr_s2 and isSubstring(s1, substr_s2):
+            return True
+    return False
+
+2. Reconstructing the String
+    - first validate input and check that it's a permutation
+    - then find the substring of "last_letter_s1 + first_letter_s1" in s2
+    - then reconstruct the string, pass it to is_substring
+        wrap_string_index + 1: + :wrap_string_index + 1: = original
+
 Edge Cases:
 - empty strings --> False
 """
@@ -45,11 +62,11 @@ Edge Cases:
 
 def is_rotation(s1: str, s2: str) -> bool:
     def isSubstring(s1: str, s2: str) -> bool:
-        pass
+        return s1 == s2
 
     def freq_dist(string) -> dict:
         dist = dict()
-        for char in list(string):
+        for char in string:
             if char not in dist:
                 dist[char] = 1
             else:
@@ -57,9 +74,8 @@ def is_rotation(s1: str, s2: str) -> bool:
         return dist
 
     def is_equal(char_dist1, char_dist2):
-        if len(char_dist1) == len(char_dist2):
+        if len(char_dist1) != len(char_dist2):
             return False
-        
         for char in char_dist1.keys():
             if char not in char_dist2 or char_dist1[char] != char_dist2[char]:
                 return False
@@ -73,13 +89,22 @@ def is_rotation(s1: str, s2: str) -> bool:
         return is_equal(char_dist1, char_dist2)
     
     # validate the input, then check if it's a permutation
+    is_rotation = False
     if (s1 and s2) and is_permutation(s1, s2):
-        # find the first letter of s1 in s2
-        first_letter_s1 = s1[0]
-        ndx_first_letter_s2 = s1.find(first_letter_s1)
-        # take the substring from there, till the end
-        substr_s2 = s2[ndx_first_letter_s2:]
-        # check if that != s1
-        if s1 != substr_s2 and isSubstring(s1, substr_s2):
-            return True
-    return False
+        # - then find the substring of "last_letter_s1 + first_letter_s1"
+        wrap_substring = s1[-1] + s1[0]
+        wrap_substring_index = s2.find(wrap_substring)
+        print(wrap_substring_index)
+        if wrap_substring_index > -1:
+            # - then reconstruct the string, pass it to is_substring
+            reconstructed_str = (
+                s2[wrap_substring_index + 1:] + s2[:wrap_substring_index + 1]
+            )
+            is_rotation = isSubstring(s1, reconstructed_str)
+    return is_rotation
+
+
+if __name__ == "__main__":
+    s1 = "waterbottle"
+    s2 = "erbottlewat"
+    print(is_rotation(s1, s2))
