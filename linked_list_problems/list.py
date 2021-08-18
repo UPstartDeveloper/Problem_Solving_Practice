bfc59ce25304ff23f67cd6f6e8bfc048a7baaddc
= <zainr7989@gmail.com>
2021-08-18 17:53:07 -0400

    Init Linked List class practice.

---
+++ linked_list_problems/list.py
@@ -0,0 +1,37 @@
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+class LinkedList:
+    """
+    Singly Linked List
+        - 1 head pointer is needed
+        - 1 tail pointer also good, to make append O(1) time
+
+    Doubly Linked List
+        - recommended to have head and tail (w/ null pointers on side)
+        - recommended  if want to design a deque
+    
+    """
+    def __init__(self):
+        pass
+
+    def add(self, new):
+        pass
+
+    def prepend(self):
+        pass
+
+    def delete(self, val):
+        pass
+
+    def find(self, val):
+        pass
+
+    def insert(self, val, steps):
+        pass
+
+    def update(self, old_val, new_val):
+        pass
