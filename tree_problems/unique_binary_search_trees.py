cf2443d4245e94c096d645c9f89aa5f69641cecf
= <zainr7989@gmail.com>
2021-11-08 12:04:27 -0500

    Optimize 'Unique BSTs' solution.

--- tree_problems/unique_binary_search_trees.py
+++ tree_problems/unique_binary_search_trees.py
@@ -1,53 +1,10 @@
-from collections import deque
-
-class TreeNode:
-    def __init__(self, val):
-        self.val = val
-        self.left, self.right = None, None
-
-class BST:
-    def __init__(self, values):
-        self.root = None
-        for val in values:
-            self.insert(val)
-        
-    def insert(self, val):
-        """ASSUME it's ok to 'insert' values already in the tree"""
-        # early exit
-        if self.root is None or self.root.val == val:
-            self.root = TreeNode(val)
-        else:
-            # make new node
-            new_node = TreeNode(val)
-            # find where to insert it
-            parent, current_node = None, self.root
-            while current_node is not None and current_node.val != val:
-                parent = current_node
-                if val < current_node.val:
-                    current_node = current_node.left
-                else:  # val > current_node.val
-                    current_node = current_node.right
-            # add the new node as a left or right child
-            if parent.left == current_node and parent.val > val:
-                parent.left = new_node
-            elif parent.right == current_node and parent.val < val:
-                parent.right = new_node
-    
-    def bfs(self):
-        visited = list()
-        if self.root is not None: 
-            # BFS
-            q = deque([self.root])
-            while q:
-                node = q.popleft()
-                visited.append(node.val)
-                for child in [node.left, node.right]:
-                    if child is not None:
-                        q.append(child)
-        return tuple(visited)
-
-
 class Solution:
+    def __init__(self):
+        self.cache = {
+            0: 1,
+            1: 1
+        }
+
     def numTrees(self, n: int) -> int:
         """
         Input:
@@ -102,37 +59,18 @@ class Solution:
             
         
         """
-        ### HELPER
-        def _generate_perms(current_perm, all_perms, choices):
-            # Base case: perm is done
-            if len(current_perm) == n:
-                all_perms.append(current_perm[:])
-            # recursive: add the next value
-            else:
-                for index, val in enumerate(choices):
-                    current_perm.append(val)
-                    remaining = [
-                        val for other_index, val in enumerate(choices)
-                        if index != other_index
-                    ]
-                    _generate_perms(current_perm, all_perms, remaining)
-                    current_perm.pop()
-            return all_perms
-        
         ### DRIVER
-        # A: init the node vals
-        node_vals = list(range(1, n + 1))
-        # B: generate a perms of node vals (list)
-        permutations = _generate_perms([], [], node_vals) 
-        # C: compute # of unique
-        unique_bsts = set()
-        for perm in permutations:
-                # 1) make a BST from each perm
-                tree = BST(perm)  # TODO
-                # 2) BFS that BST
-                node_ordering = tree.bfs()  # TODO - return tuple(int)
-                # 3) add the output of the BFS to a set of tuples
-                unique_bsts.add(node_ordering)
-        # D: return the length of set
-        return len(unique_bsts)
- 
+        if n not in self.cache:
+            count = 0 
+            for start in range(1, n + 1):
+                num_left_children, num_right_children = (
+                    start - 0 - 1,
+                    n - start
+                )
+                count += (
+                    self.numTrees(num_left_children) *
+                    self.numTrees(num_right_children)
+                )
+            self.cache[n] = count
+        return self.cache[n]
+     
