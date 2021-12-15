f23ffcfd5df65052dc3a3955943eb662f1c8d861
Zain Raza <zainr7989@gmail.com>
2021-12-15 13:58:23 -0500

    Update smallest_string_with_swaps.py

--- graph_problems/smallest_string_with_swaps.py
+++ graph_problems/smallest_string_with_swaps.py
@@ -17,27 +17,56 @@ class CharacterGraph:
         # add the edges between the vertices
         for index1, index2 in pairs:
             v1, v2 = self.characters[index1], self.characters[index2]
-            v1.neighbors[v2.index] = v2
-            v2.neighbors[v1.index] = v1
+            v1.neighbors[v2.ndx] = v2
+            v2.neighbors[v1.ndx] = v1
             
     def sort_comps(self) -> List[str]:
-        # A: init empty array of index --> char
- 
-        # B: for each comp
-            # 1: get all chars in the comp - DFS/BFS
-            # 2: get all the indices in the comp
-            # 3: sort 'em and zip 'em
-            # 4: add them to the overall array
-            
-        # C: fill in any missing values in the array using s
         
+        def _dfs(visited, vertex):
+            comp_visited = set()
+            stack = [vertex]
+            while stack:
+                v = stack.pop()
+                # visit
+                comp_visited.add(v)
+                # add neighbors
+                for neighbor in v.neighbors.values():
+                    if neighbor not in comp_visited:
+                        stack.append(neighbor)
+            # now get the smallest
+            chars = [v.cc for v in comp_visited]
+            indices = [v.ndx for v in comp_visited]
+            # update global set
+            visited.union(set([v.ndx for v in comp_visited]))
+            return dict(zip(sorted(indices), sorted(chars)))
+        
+        # A: init empty array of index --> char
+        sorted_chars = sc = ['' for _ in range(len(self.string))]
+        # B: for each comp
+        visited = set()
+        index = 0
+        while index < len(self.string):
+            if index not in visited:
+                vertex = self.characters[index]
+                # 1: get all chars + indices in the comp - DFS/BFS
+                smallest_order = _dfs(visited, vertex)
+                # 3: sort 'em and zip 'em
+                # smallest_order = dict(zip(sorted(indices), sorted(chars)))
+                # 4: add them to the overall array
+                for char_index in smallest_order:
+                    sc[char_index] = smallest_order[char_index]
+            index += 1
+        # C: fill in any missing values in the array using s
+        for index in range(len(sc)):
+            if sc[index] == '':
+                sc[index] = self.string[index]
         # D: return the array
-        pass
+        return sc
 
 class Solution:
     def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
         """
-        Leetcode: https://leetcode.com/problems/smallest-string-with-swaps/
+        Leetcode: https://leetcode.com/problems/smallest-string-with-swaps/ 
         
         Input/Problem:
             non empty - only English, immutable, lowercase
