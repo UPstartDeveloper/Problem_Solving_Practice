9f9db79126b184a19ab3545282ad22574265fa8d
Zain Raza <zainr7989@gmail.com>
2022-04-18 11:35:23 -0400

    Solve "Kth Smallest Element in a BST"

---
+++ tree_problems/kth_smallest_value_in_bst.py
@@ -0,0 +1,53 @@
+from typing import Optional
+
+class TreeNode:
+    """Definition for a binary tree node."""
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        """
+        LeetCode: https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+        
+        Intuition:
+            in order DFS
+            sorting
+            
+        Assume:
+            k <= n
+            both are pos
+            
+        EC:
+            null root --> raise ValueError
+            invalid tree --> N/A
+            
+        Approach:
+            do k visits in DFS
+                on the kth visit, return
+        
+        """
+        ### HELPERS
+        def _find_kth_smallest(node, visits=0):
+            """in-order DFS, recursively"""
+            if node and visits < k:
+                visits = _find_kth_smallest(node.left, visits)
+                # visit
+                visits += 1
+                if visits == k:
+                    self.kth = node.val
+                if visits < k:
+                    visits = _find_kth_smallest(node.right, visits)
+            return visits
+        
+        ### DRIVER
+        # A: init return value
+        self.kth = -1
+        # B: find kth-smallest
+        if root:
+            _find_kth_smallest(root)
+        # C: return
+        return self.kth
