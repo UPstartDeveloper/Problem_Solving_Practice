c6d59066cf7726d8ee41590894d228b9da137c94
Zain Raza <zainr7989@gmail.com>
2022-04-25 10:58:10 -0400

    Solve "Peeking Iterator"

---
+++ miscellaneous/peeking_iterator.py
@@ -0,0 +1,72 @@
+# Below is the interface for Iterator, which is already defined for you.
+#
+# class Iterator:
+#     def __init__(self, nums):
+#         """
+#         Initializes an iterator object to the beginning of a list.
+#         :type nums: List[int]
+#         """
+#
+#     def hasNext(self):
+#         """
+#         Returns true if the iteration has more elements.
+#         :rtype: bool
+#         """
+#
+#     def next(self):
+#         """
+#         Returns the next element in the iteration.
+#         :rtype: int
+# """
+
+from collections import deque
+
+
+class PeekingIterator:
+    def __init__(self, iterator):
+        """
+        LeetCode: https://leetcode.com/problems/peeking-iterator/
+        
+        Initialize your data structure here.
+        :type iterator: Iterator
+        
+        ASSUME: 
+            iterator not empty 
+            all calls to next() and hasNext() are valid
+            
+        EC:
+            1) multiple calls to peek() in a row?
+        """
+        self.iter = iterator
+        self.cache = deque()  # FIFO
+        
+    def peek(self):
+        """
+        Returns the next element in the iteration without advancing the iterator.
+        :rtype: int
+        """
+        if len(self.cache) == 0:
+            current_elem = self.iter.next()
+            self.cache.append(current_elem)
+        return self.cache[0]
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        if len(self.cache) > 0:
+            return self.cache.popleft()
+        return self.iter.next()
+        
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        return len(self.cache) > 0 or self.iter.hasNext()
+        
+
+# Your PeekingIterator object will be instantiated and called as such:
+# iter = PeekingIterator(Iterator(nums))
+# while iter.hasNext():
+#     val = iter.peek()   # Get the next element but not advance the iterator.
+#     iter.next()         # Should return the same value as [val].
