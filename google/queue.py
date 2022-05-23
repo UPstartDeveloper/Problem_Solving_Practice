64d93a921c634526b3f20f524bc102b8ffb4b3a2
= <zainr7989@gmail.com>
2022-05-23 18:57:39 -0400

    Add Queue classes.

---
+++ google/queue.py
@@ -0,0 +1,41 @@
+from list import LinkedList, ListNode
+
+
+class ArrayQueue:
+    """going to choose the last index and the back, and first as front
+       in prod this choice would most likely depend on the application domain
+    """
+    def __init__(self):
+        self.items = list()
+
+    def enqueue(self, new):
+        self.items.append(new)
+
+    def dequeue(self):
+        if self.items:
+            return self.items.pop(0)
+
+    def front(self):
+        if self.items:
+            return self.items[0]
+
+
+class LinkedQueue:
+    """front of the queue is the head, back is the tail"""
+    def __init__(self):
+        self.items = LinkedList()
+
+    def enqueue(self, new):
+        new_node = ListNode(new)
+        self.items.add(new_node, front=False)
+
+    def dequeue(self):
+        if self.items.size > 0:
+            first = self.items.head
+            deleted = self.items.delete(first)
+            if deleted is not None:
+                return deleted.val
+
+    def front(self):
+        if self.items.size > 0:
+            return self.items.head.val
