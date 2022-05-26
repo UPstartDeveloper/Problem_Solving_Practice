efd63844699996ef692d1185f9e85d9e43e46e87
= <zainr7989@gmail.com>
2022-05-26 19:07:48 -0400

    Practice memoization

---
+++ google/memo.py
@@ -0,0 +1,23 @@
+class memoize:
+    """class-based way to do memoization"""
+    def __init__(self, func):
+        self.cache = dict()
+        self.f = func
+
+    def __call__(self, *args):
+        if args not in self.cache:
+            self.cache[args] = self.f(args)
+        return self.cache[args]
+
+
+"""func-based way"""
+def memoize(func):
+    cache = dict()
+
+    def caller(*args):
+        if args not in cache:
+            cache[args] = func(args)
+        return cache[args]
+
+    return caller
+
