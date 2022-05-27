895029aa8f1cf73cf720482f92f9a7271ffb249d
Zain Raza <zainr7989@gmail.com>
2022-05-27 17:10:48 -0400

    Solve "Number of Steps to Reduce a Number to Zero"

---
+++ bit_manipulation_problems/number_of_steps_to_reduce_number_to_zero.py
@@ -0,0 +1,18 @@
+class Solution:
+    """LeetCode: https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/"""
+    def numberOfSteps(self, num: int) -> int:
+        if not isinstance(num, int) or num < 0:
+            raise ValueError(f"value {num} is not an int > 0")
+            
+        steps = 0
+        
+        bits = bin(num)[2:]
+        
+        for bit in bits:
+            if bit == '0':
+                steps += 1
+            else:  # bit is 1
+                steps += 2
+        
+        return steps - 1  # readjust, b/c last 0 doesn't need to go away
+        
