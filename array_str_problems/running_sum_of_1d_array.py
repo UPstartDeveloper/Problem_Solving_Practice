d181b1a3b1f9ebc6dfa74ceb1f235e993c37d030
Zain Raza <zainr7989@gmail.com>
2022-06-01 17:07:06 -0400

    Solve "Running Sum of 1D Array"

---
+++ array_str_problems/running_sum_of_1d_array.py
@@ -0,0 +1,32 @@
+from typing import List
+
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        """
+        LeetCode: https://leetcode.com/problems/running-sum-of-1d-array/
+        
+        input:
+            int[pos/neg, unsorted, len > 0, dupes]
+            assume it's mutable
+            
+        Output: cumsum
+        
+        Intuition - for loop
+        
+        EC: 
+            empty array - [0]
+            anything out of range - ValueError
+            
+        [-9, -6,    0,   5, -3,  4, 4]
+        [-9, -15, -15, -10, -7, -3  1]
+            
+        """
+        sum_so_far = ssf = 0
+        
+        for index, num in enumerate(nums):
+            
+            ssf += num
+            nums[index] = ssf
+        
+        return nums
