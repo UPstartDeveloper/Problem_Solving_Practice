ef140dad36b2570a251319357eee9c0540b89a73
Zain Raza <zainr7989@gmail.com>
2022-05-31 21:34:29 -0400

    Solve "Check If a String Contains All Binary Codes of Size K"

---
+++ bit_manipulation_problems/check_if_a_contains_all_binary_permutations_of_size_k.py
@@ -0,0 +1,19 @@
+class Solution:
+    def hasAllCodes(self, s: str, k: int) -> bool:
+        """
+        O(n*k) time, O(2**k) space
+        LeetCode: https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/
+        """
+        all_substrs, all_codes = set(), (2 ** k)
+        
+        # TODO-optimize: use bits as base10, slide across for unique
+        for index in range(0, len(s) - k + 1):
+            substr = s[index:index + k]
+            all_substrs.add(substr)
+            
+            # early exit
+            if len(all_substrs) == all_codes:
+                return True
+            
+        # final answer
+        return len(all_substrs) == all_codes 
